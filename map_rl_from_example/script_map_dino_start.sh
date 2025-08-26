62069b66105c2e824b458ac2e17383c85f6874b7
suk063 <suk063@ucsd.edu>
2025-08-25 22:45:01 -0700

    feat: map feature modulation

--- map_rl_from_example/script_map_dino_start.sh
+++ map_rl_from_example/script_map_dino_start.sh
@@ -1,7 +1,7 @@
 python map_rl_from_example/ppo_map.py \
     --env_id=PickYCBSequential-v1 \
     --control_mode=pd_joint_vel \
-    --exp_name=YCB_sequential_xarm6_ppo_map_cnn \
+    --exp_name=YCB_sequential_xarm6_ppo_map_dino_start \
     --num_envs=100 \
     --num_eval_envs=20 \
     --eval_freq=20 \
@@ -11,9 +11,10 @@ python map_rl_from_example/ppo_map.py \
     --gamma=0.9 \
     --ent_coef=1e-3 \
     --learning_rate=3e-4 \
-    --vision_encoder=plain_cnn \
+    --vision_encoder=dino \
+    --freeze_dino_backbone \
     --capture-video \
     --track \
     --wandb_project_name "PPO-RL-Map" \
     --use_map \
-    # --activate_map
+    --start_condition_map
