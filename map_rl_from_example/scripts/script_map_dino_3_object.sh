abe2257ca6d20917c21b3a9ef61a4b43327d9234
suk063 <suk063@ucsd.edu>
2025-09-01 19:13:43 -0700

    update scripts

--- map_rl_from_example/scripts/script_map_dino_3_object.sh
+++ map_rl_from_example/scripts/script_map_dino_3_object.sh
@@ -1,21 +1,20 @@
 python map_rl_from_example/ppo_map.py \
     --env_id=PickYCBSequential-v1 \
     --control_mode=pd_joint_vel \
-    --exp_name=YCB_sequential_xarm6_ppo_map_dino_zero_1 \
+    --exp_name=YCB_sequential_xarm6_ppo_map_dino_zero_3_object \
     --num_envs=100 \
-    --num_eval_envs=12 \
+    --num_eval_envs=20 \
     --eval_freq=20 \
     --total_timesteps=50_000_000 \
     --num_steps=200 \
     --num_eval_steps=200 \
     --gamma=0.9 \
     --learning_rate=3e-4 \
-    --ent_coef=1e-3 \
     --vision_encoder=dino \
     --capture-video \
     --track \
     --wandb_project_name "PPO-RL-Map" \
     --use_map \
-    --checkpoint "save_checkpoint/ckpt_61.pt" \
-    # --load_actor_logstd
+    --object_num=3
+
     # --start_condition_map
